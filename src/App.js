421568b31e74b5d8071e0f14d8a4398e4c7559e0
tulleycodes <71989325+two23three@users.noreply.github.com>
2024-05-12 18:03:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <main>
+      <h1>Hi, I'm (your name)</h1>
+      <img alt="My profile pic" src="https://via.placeholder.com/350" />
+      <h2>About Me</h2>
+      <p>
+      hellooo
+      <div>
+        <a href="https://github.com">GitHub</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+      </div>
+    </main>
+  );
 }
 
 export default App;
